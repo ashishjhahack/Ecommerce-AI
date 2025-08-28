6be4ab834220e1d0b3aa9111d744c2cd7f39970e
Ashish Jha <ashishjhag143@gmail.com>
2025-08-28 17:08:54 +0530

    adding scroll velocity feature

--- frontend/src/components/Chatbot/ChatbotWidget.jsx
+++ frontend/src/components/Chatbot/ChatbotWidget.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import ChatWindow from "./ChatWindow";
+import { gsap } from "gsap";
+
 
 const ChatbotWidget = () => {
+
+  const AIRef = useRef(null);
+
+  useEffect(() => {
+    const t1 = gsap.timeline();
+    gsap.fromTo(AIRef.current,
+      {scale: 0, opacity: 0},
+      {scale: 1,opacity: 1, duration: 2, ease: "power3.out"}
+    )
+
+  }, [])
+
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <div
+      <div ref={AIRef}
         className="fixed bottom-5 right-5 bg-gray-800 w-30 border-black border-2  text-white p-4 rounded-full cursor-pointer shadow-lg"
         onClick={() => setIsOpen(!isOpen)}
       >
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,11 +5,17 @@ import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
 import TopFeatures from '@/components/TopFeatures'
+import ScrollVelocity from '../components/ScrollVelocity';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <ScrollVelocity
+        texts={[".Shop Smart", ".Best Sellers"]}
+        
+        className="custom-scroll-text"
+      />
       <TopFeatures />
       <LatestCollection />
       <BestSeller />
