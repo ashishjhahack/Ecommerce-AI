82b9ac606c63e758e36b4f3e299e9d6195a4d415
Ashish Jha <ashishjhag143@gmail.com>
2025-09-11 17:06:54 +0530

    some changes

--- admin/src/pages/LIst.jsx
+++ admin/src/pages/LIst.jsx
@@ -8,7 +8,7 @@ const List = ({token}) => {
 
   const fetchList = async () => {
     try {
-      const response = await axios.get(`${backendUrl}/api/product/list`);
+      const response = await axios.get(`${backendUrl}/api/product/list`)
       if(response.data.success){
         setList(response.data.products);
       }
